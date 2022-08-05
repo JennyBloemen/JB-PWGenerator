bd72a8dca34b767c5ae0f6a0fc1b908bfef8028c
Jenny Bloemen <jennyjbloemen@gmail.com>
2022-08-04 22:49:24 -0600

    almost there

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,7 @@ var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
 var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var options = []
 // variables for confirms, alerts adn functions
 var usenumbers;
 var usespecialChar;
@@ -27,61 +28,23 @@ function getPasswordOptions() {
   usealphaLower = confirm('Click OK to include lowercase characters.');
   usealphaUpper = confirm('Click OK to include uppercase characters.');
   
-  // forces user to start over if no options are chosen
-  if(!usenumbers && !usespecialChar && !usealphaLower && !usealphaUpper) {
-    alert("You must choose one option, please try again.");
-  }
-  // Stores user choices
-  var passwordOptions = {pwlength, usespecialChar, usenumbers, usealphaLower, usealphaUpper,
-  };
-  return passwordOptions;
-}
-
-// allows random user choices
-function getRandom(arr) {
-  var randIndex = Math.floor(Math.random() * arr.length);
-  var randElement = arr[randIndex];
-
-  return randElement;
-}
-
-// Function to generate password with user choices
-function generatePassword() {
-  var options = getPasswordOptions();
-  var choices = [];
-  var pwoptions = [];
-  var userchoice = [];
-
-// user choices
-  if (options.usespecialChar) {
-    pwoptions = pwoptions.concat(specialChar);
-    userchoice.push(getRandom(specialChar));
-  }
-  if (options.usenumbers) {
-    pwoptions = pwoptions.concat(numbers);
-    userchoice.push(getRandom(numbers));
-  }
-  if (options.usealphaLower) {
-    pwoptions = pwoptions.concat(alphaLower);
-    userchoice.push(getRandom(alphaLower));
-  }
-  if (options.usealphaUpper) {
-    pwoptions = pwoptions.concat(alphaUpper);
-    userchoice.push(getRandom(alphaUpper));
-  }
-
-  // For loop to make user choices random
-  for (var i = 0; i < options.pwlength; i++) {
-    passwordOptions = getRandom(pwoptions);
-    choices.push(passwordOptions);
-  }
+  // Adds user choices to an array
+  if(usenumbers){options = options.concat(numbers)}
+  if(usespecialChar){options = options.concat(specialChar)}
+  if(usealphaLower){options = options.concat(alphaLower)}
+  if(usealphaUpper){options = options.concat(alphaUpper)}
   
-  // for loop to include user choices
-  for (var i = 0; i < userchoice.pwlength; i++) {
-    choices[i] = userchoice[i];
-  }
-  return choices.join('');
+  return options;
 }
+
+// I am struggling to figure this section out and will need to request help from a ta
+// Function for getting a random element from an array
+// function getRandom(arr) {
+//   var randIndex = Math.floor(Math.random() * arr.pwlength);
+//   var randElement = arr[randIndex];
+
+//   return randElement;
+// }
 
 // Assignement Code Provided
 var generateBtn = document.querySelector('#generate');
@@ -90,6 +53,7 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector('#password');
 
-  passwordText.value = password;
+  passwordText.value = password;}
+
+  generateBtn.addEventListener('click', writePassword);
 }
-generateBtn.addEventListener('click', writePassword);
